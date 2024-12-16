2d0767bb83fd21212b88ae2e83d95996f0705e1d
davidmugisho <davidmugisho243@gmail.com>
2024-12-16 19:28:46 +0200

    verifi

--- app/verifyphone.tsx
+++ app/verifyphone.tsx
@@ -12,17 +12,19 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 export default function verifyPhoneScreen () {
     return(
       <View style={styles.container}>
-        <Text>Verify your number</Text>
-        <Text>Enter verification code sent to 32842224848</Text>
-        <Pressable style={styles.button}>
-          <Text style={styles.buttontext}>Verification code</Text>
+        <Text style={styles.text}>Verify your number</Text>
+        <Text style={styles.text1}>Enter verification code sent to{' '}
+          <Text style={{color:'blue'}}>32842224848</Text>
+        </Text>
+        <Pressable style={styles.inputContainer}>
+          <Text style={styles.buttonText}>Verification code</Text>
         </Pressable>
         <View style={styles.buttonContainer}>
-            <Link href="/setpasswordNumber" style={styles.button}>
-              <Text style={styles.buttonText}>Continue</Text>
+            <Link href="/setpasswordNumber">
+              <Text style={styles.buttonText1}>Continue</Text>
             </Link>
           </View>
-          <Text>Haven’t got the code? Resend code</Text>
+          <Text style={styles.text2}>Haven’t got the code? Resend code</Text>
       </View>
   );
 };
@@ -38,16 +40,60 @@ const styles = StyleSheet.create({
     alignItems: 'center', // Center items horizontally
     borderRadius: 16, // Optional: Rounded corners
     },
-    button:{
-
+    text:{
+    fontSize:45,
+    lineHeight: 48,
+    fontWeight: '700',
+    color: '#141718',
     },
-    buttontext:{
-
+    text1: {
+      fontSize: 14,
+      fontWeight: '500',
+      lineHeight: 24,
+      color: '#141718',
+      fontFamily: 'Inter',
+    },
+    inputContainer: {
+      // flexDirection: 'row',
+      alignItems: 'flex-start',
+      justifyContent:'center',
+      borderRadius: 12,
+      paddingHorizontal: 12,
+      width: 311,
+      height: 60,
+      backgroundColor: '#F3F5F7',
+    },
+    text2:{
+      fontSize:14,
+      lineHeight:24,
+      fontWeight:500,
+      fontFamily:'Inter',
+      letterSpacing:0.12,
+      color:'#141718'
+    },
+    buttonText1:{
+      color: '#FEFEFE',
+      fontSize: 16,
+      fontWeight: '600',
+      lineHeight: 24,
+      fontFamily: 'Inter',
+      letterSpacing: 0.24,
     },
     buttonContainer:{
-
+      backgroundColor: '#020A3C',
+      width: 311,
+      height: 60,
+      borderRadius: 12,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
     buttonText:{
+      fontFamily:'Inter',
+      fontWeight:500,
+      fontSize:14,
+      lineHeight:24,
+      letterSpacing:0.12,
+      color:'#6C7275'
 
     }
 
