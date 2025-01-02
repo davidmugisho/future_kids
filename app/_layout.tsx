0d57d9965e67523e8058833a68931a39d0ce8a44
davidmugisho <davidmugisho243@gmail.com>
2025-01-02 14:53:08 +0200

    import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,13 @@ import phoneSignScreen from "./phoneSign_up";
 export default function RootLayout() {
   return (
     <>
+
     <StatusBar style="light"/>
     <Stack>
   <Stack.Screen name="index" options={{ headerShown:false}}  />
   <Stack.Screen name="splash" options={{ title:'Splash', headerShown:false }}/>
-  <Stack.Screen name="onboarding1" options={{ title:'onboarding1', headerShown:false , headerLeft:() => <></>}}/>
-  <Stack.Screen name="onboarding2" options={{ title:'onboarding2', headerShown:false , headerLeft:() => <></> }}/>
+  <Stack.Screen name="onboarding1" options={{ title:'onboarding1', headerShown:false , }}/>
+  <Stack.Screen name="onboarding2" options={{ title:'onboarding2', headerShown:false , headerLeft:() => <> </> }}/>
   <Stack.Screen name="sign_up" options={{ title:'Sign up' ,headerShown: false,  headerLeft : () => <></>}}/>
   <Stack.Screen name="phoneSign_up" options={{ title: 'phoneSign up', headerShown: false , }}/>
   <Stack.Screen name="verifyphone" options={{ headerShown: false , headerLeft:() => <></> }} />
