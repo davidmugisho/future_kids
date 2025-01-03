c3dd9cea00afad8897de38ad09c639a8bc776f91
davidmugisho <davidmugisho243@gmail.com>
2025-01-03 12:45:13 +0200

    resetscreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import {Tabs} from "expo-router";
 import phoneSignScreen from "./phoneSign_up";
+import React from "react";
 
 export default function RootLayout() {
   return (
@@ -12,7 +13,7 @@ export default function RootLayout() {
   <Stack.Screen name="index" options={{ headerShown:false}}  />
   <Stack.Screen name="splash" options={{ title:'Splash', headerShown:false }}/>
   <Stack.Screen name="onboarding1" options={{ title:'onboarding1', headerShown:false , }}/>
-  <Stack.Screen name="onboarding2" options={{ title:'onboarding2', headerShown:false , headerLeft:() => <> </> }}/>
+  <Stack.Screen name="onboarding2" options={{ title:'onboarding2', headerShown:false , headerLeft:() => <></> }}/>
   <Stack.Screen name="sign_up" options={{ title:'Sign up' ,headerShown: false,  headerLeft : () => <></>}}/>
   <Stack.Screen name="phoneSign_up" options={{ title: 'phoneSign up', headerShown: false , }}/>
   <Stack.Screen name="verifyphone" options={{ headerShown: false , headerLeft:() => <></> }} />
@@ -23,6 +24,7 @@ export default function RootLayout() {
   <Stack.Screen name="sign_in" options={{ title:'Sign in' ,headerShown: false,  headerLeft : () => <></>}}/>
   <Stack.Screen name="Sign_in_phone" options={{ headerShown: false }} />
   <Stack.Screen name="Sign_in_email" options={{ headerShown: false }} />
+  <Stack.Screen name="reset_password" options={{headerShown : false}} />
 
 
 
